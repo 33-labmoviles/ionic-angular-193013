06803df18d78a849c92bfa0bf58eb1a7df8ea0bf
Diego Jasso <jassito2002@gmail.com>
2022-03-05 15:37:37 -0600

    Practica 4

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input , OnChanges} from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
@@ -8,5 +8,78 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+    alumnos= [
+     {
+       "nombre": "Diego",
+       "apellido": "Jasso",
+       "matricula" : "123ABC"
+     },
+     {
+      "nombre": "Sergio",
+      "apellido": "Gutierrez",
+      "matricula" : "123ABC"
+    },
+    {
+      "nombre": "Luis",
+      "apellido": "Tamez",
+      "matricula" : "123ABC"
+    },
+    {
+      "nombre": "Abraham",
+      "apellido": "Moreno",
+      "matricula" : "123ABC"
+    },
+    {
+      "nombre": "Aylin",
+      "apellido": "Demetci",
+      "matricula" : "123ABC"
+    },
+    {
+      "nombre": "Luis",
+      "apellido": "Martinez",
+      "matricula" : "123ABC"
+    },
+    {
+      "nombre": "Manuel",
+      "apellido": "Juarez",
+      "matricula" : "123ABC"
+    },
+    {
+      "nombre": "Brian",
+      "apellido": "Esquivel",
+      "matricula" : "123ABC"
+    },
+    {
+      "nombre": "Roguel",
+      "apellido": "Guel",
+      "matricula" : "123ABC"
+    },
+    {
+      "nombre": "Luis",
+      "apellido": "Villareal",
+      "matricula" : "123ABC"
+    },
+    {
+      "nombre": "Omar",
+      "apellido": "Garza",
+      "matricula" : "123ABC"
+    },
+    ];
+  
 
-}
+  @Input() nombre: string = "";
+  @Input() apellido: string = "";
+  @Input() matricula: string = "";
+
+  agregar_alumno(): void{
+    var nuevoAlumno : any = {
+      "nombre": this.nombre,
+      "apellido": this.apellido,
+      "matricula": this.matricula
+    }
+    this.alumnos.push(nuevoAlumno);
+    this.alumnos.sort();
+    console.log(this.alumnos);
+    
+  }
+}  
